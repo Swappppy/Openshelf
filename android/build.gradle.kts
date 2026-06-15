f54dd04b301765f2d42ff1ee59bd258b06c0c857
Swappppy <francft@tuta.com>
2026-06-15 18:24:46 +0200

    Java 8 to 17

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,6 +16,19 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            android.compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
