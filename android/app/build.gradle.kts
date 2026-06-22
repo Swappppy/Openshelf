ccf519f9a246da585ab3becf670ce6cf727cb063
Swappppy <francft@tuta.com>
2026-06-22 19:05:29 +0200

    Apply F-Droid Flutter template: --split-per-abi, VercodeOperation, UpdateCheckData

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,12 +22,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    packagingOptions {
-        exclude("lib/armeabi-v7a/**")
-        exclude("lib/x86_64/**")
-        exclude("lib/x86/**")
-    }
-
     kotlin {
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
